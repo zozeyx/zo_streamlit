619adb029252ae55644b14cf17feb7977029ee66
zozeyx <126737048+zozeyx@users.noreply.github.com>
2023-05-16 12:58:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 
-df = pd.DataFrame(np.random.randn(500,2) / [50,50] + [37.76, -122.4],columns = ['lat', 'lon'])
-st.map(df)
-                                                                       
+st.text_input('ID')
+st.text_input('Password')
+st.text_input('First Name')
+st.text_input('Middle Name')
+st.number_input('Pick your age', 0, 100)
+st.radio('Pick your gender', ['Male', 'Female'])
+
+
